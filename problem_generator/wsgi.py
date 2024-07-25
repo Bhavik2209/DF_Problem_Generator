13f9c238db2fb81feaebc6eac37a1af7ed26210b
Bhavik2209 <bhavikrohit22@gmail.com>
2024-07-25 23:06:08 +0530

    add

--- problem_generator/wsgi.py
+++ problem_generator/wsgi.py
@@ -8,11 +8,17 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+from django.core.management import execute_from_command_line
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'problem_generator.settings')
 
 application = get_wsgi_application()
 
-app = application
+
+
+if __name__ == "__main__":
+    execute_from_command_line(sys.argv)
